4c78545f46b1f202e3c5a12d2d47839ce1ea83bf
Shannya <niveyro101@gmail.com>
2023-03-14 04:08:19 -0400

    issues with role(s) table

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,31 @@
-IF EXISTS DROP DATABASE hiring_db;
+DROP DATABASE IF EXISTS hiring_db;
 CREATE DATABASE hiring_db;
 USE hiring_db;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
-)
+    names VARCHAR(30) NOT NULL
+);
+DESCRIBE * FROM department;
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
+    department_id INT,
     FOREIGN KEY (department_id)
-    REFERENCE department(id)
-)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+DESCRIBE * FROM roles;
 CREATE TABLE employee (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    FOREIGN KEY role_id
-    REFERENCE role(id)
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
     ON DELETE SET NULL
-)
+);
+DESCRIBE * FROM employee;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO department(names)
+    VALUES ("dresses"),("shoes"),("coats");
+SELECT * FROM department;
+
+INSERT INTO roles(title)
+    VALUES("manager", "40000","4");
